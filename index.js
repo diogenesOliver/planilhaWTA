49d3db79260ed19caae9c83dd0c71ca810a1de4d
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-08-24 18:34:05 +0000

    Creating a new route

--- index.js
+++ index.js
@@ -3,10 +3,26 @@ require('dotenv').config()
 let express = require('express')
 let app = express()
 
-const PORT = 8080
+const TransacoesRepositorio = require('./controller/transacoes-repositorio.js')
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/nav.html')
+const PORT = process.env.PORT
+
+app.use(express.static((`${__dirname}/public/nav.html`)))
+
+app.get('/transacoes', (req, res) => {
+    const repositorio = new TransacoesRepositorio()
+    const transacoes = repositorio.listarTransacoes()
+    res.send(transacoes)
+})
+
+app.get('/criar-transacao', (req, res) => {
+    const repositorio = new TransacoesRepositorio()
+    const transacao = {
+        valor: 30,
+        descricao: 'Bolo'
+    }
+    repositorio.criarTransacao(transacao)
+    res.status(200).send(transacao)
 })
 
 app.listen(PORT, () => {
