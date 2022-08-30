65c241784503dd723b6b704be15e71a414915792
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-08-30 19:32:00 +0000

    Getting data from database

--- infra/sql-transacoes-repositorio.js
+++ infra/sql-transacoes-repositorio.js
@@ -1,14 +1,19 @@
+require('dotenv').config()
+
 const { Pool } = require('pg')
 const pool = new Pool()
-
-
 
 class SqlTransacoesRepositorio {
 
     async listarTransacoes() {
-        pool.query
 
-        return transacoes
+        const resultado = await pool.query('SELECT * FROM transacoes')
+        console.log(resultado.rows)
+
+        return {
+            transacoes: resultado.rows
+        }
+
     }
 
     async criarTransacao(transacao) {
