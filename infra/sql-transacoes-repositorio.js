3a249fac5c615db549d80091ff73f9ac4cefa309
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-08-31 18:49:15 +0000

    Fixing the BUG on infra sql-transacoes-repositorio

--- infra/sql-transacoes-repositorio.js
+++ infra/sql-transacoes-repositorio.js
@@ -19,13 +19,13 @@ class SqlTransacoesRepositorio {
     async criarTransacao(transacao) {
         
         const consulta = `INSERT INTO
-        transacoes(valor, descricao, categoria)
+        planilha(valor, descricao, categoria)
         VALUES ($1, $2, $3) RETURNING *`
 
         const valores = [
             transacao.valor,
             transacao.descricao,
-            transacao.categria
+            transacao.categoria
         ]
 
         await pool.query(consulta, valores)
