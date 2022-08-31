6e4f92e5ba8fad42c2400984bf484c7aa4621099
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-08-31 18:35:31 +0000

    New PORT of Server

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let app = express()
 
 const TransacoesRepositorio = require('./infra/sql-transacoes-repositorio.js')
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 
 app.use(express.json())
 app.use(express.static((`${__dirname}/public`)))
